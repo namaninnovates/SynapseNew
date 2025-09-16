a0a6ec7e1221f55fa6c962bb142b35a38fbda852
Naman Gupta <157508788+namaninnovates@users.noreply.github.com>
2025-09-16 16:55:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 👇 important for Netlify
+  base: "./",
 });
